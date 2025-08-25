0e29015d0914757b36af22d168fda4cac9035195
sage-etcher <sage.message@email.com>
2025-08-25 11:54:26 -0400

    seperate the nsae core logic from the control interface (nsaectl)

---
+++ src/nsaectl/nsaectl.c
@@ -0,0 +1,18 @@
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+
+int
+main (int argc, char **argv)
+{
+    fprintf (stdout, "unimplimented\n");
+    return 0;
+}
+
+/* end of file */
