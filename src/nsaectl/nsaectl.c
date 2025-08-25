08b4403cc48a771e3d59e1d2552cfc225682e9d6
sage-etcher <sage.message@email.com>
2025-08-25 16:21:46 -0400

    add basic argc check

--- src/nsaectl/nsaectl.c
+++ src/nsaectl/nsaectl.c
@@ -27,15 +27,50 @@ main (int argc, char **argv)
         [NSAE_CMD_STATUS]     = "status",
     };
 
+    int cmd_min_args[NSAE_CMD_COUNT] = {
+        [NSAE_CMD_EXIT]       = 2,
+        [NSAE_CMD_RESTART]    = 2,
+        [NSAE_CMD_PAUSE]      = 2,
+        [NSAE_CMD_CONTINUE]   = 2,
+        [NSAE_CMD_EJECT_FD]   = 3,
+        [NSAE_CMD_LOAD_FD]    = 4,
+        [NSAE_CMD_SAVE_FD]    = 4,
+        [NSAE_CMD_EJECT_HD]   = 2,
+        [NSAE_CMD_LOAD_HD]    = 3,
+        [NSAE_CMD_SAVE_HD]    = 3,
+        [NSAE_CMD_STEP]       = 2,
+        [NSAE_CMD_RUN]        = 2,
+        [NSAE_CMD_BREAKPOINT] = 3,
+        [NSAE_CMD_STATUS]     = 2,
+    };
+
     uint8_t cmd = 0;
     for (; cmd < NSAE_CMD_COUNT; cmd++)
     {
         if (0 == strcmp (argv[1], cmds[cmd])) break;
     }
 
-    if (cmd == NSAE_CMD_COUNT)
+    if ((cmd == NSAE_CMD_COUNT) || (cmd_min_args[cmd] != argc))
     {
-        fprintf (stderr, "usage: nsaectl mode [mode_data...]\n");
+        fprintf (stderr, 
+                "usage: nsaectl mode [mode_data...]\n"
+                "\n"
+                "modes:\n"
+                "  exit\n"
+                "  restart\n"
+                "  pause\n"
+                "  continue\n"
+                "  eject_fd <fd_num>\n"
+                "  load_fd <fd_num> <file>\n"
+                "  save_fd <fd_num> <file>\n"
+                "  eject_hd\n"
+                "  load_hd <file>\n"
+                "  save_hd <file>\n"
+                "  step\n"
+                "  run\n"
+                "  breakpoint <abs_address>\n"
+                "  status\n"
+        );
         return 1;
     }
 
